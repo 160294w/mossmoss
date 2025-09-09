be103db1ff25e64b1fdbd3481c6532449a87ba29
160294w <160294w@gmail.com>
2025-09-09 09:16:40 +0900

    アスキーアート生成ツールを実装
    機能:
    - 2ちゃんねる風アスキーアートのランダム生成
    - カテゴリ別分類（キャラクター、感情、1行表現）
    - 豊富なAAコレクション（モナー、やる夫、ギコ、オエー、麻呂など）
    - 汎用性の高い1行表現を多数収録
    - カテゴリフィルタリング機能
    - ワンクリックコピー機能
    - レスポンシブデザイン
    
    コンテンツ:
    - 基本キャラクター: モナー、やる夫、ギコ、オエー、麻呂
    - 感情表現: 喜怒哀楽の多彩なパターン
    - 1行表現: ｷﾀ━━━(ﾟ∀ﾟ)━━━!!、orz、(´･ω･`)ショボーン等
    - ネットスラング: うはｗｗｗおkｗｗｗ、ワロスwww等
    - 40+種類の表現パターンを収録
    
    テクニカル実装:
    - テンプレートリテラル内バッククォートのエスケープ処理
    - ランダム選択アルゴリズム
    - カテゴリベースフィルタリング

---
+++ src/components/Tools/AsciiArtGenerator.tsx
@@ -0,0 +1,640 @@
+import { useState } from 'react';
+import { Button } from '../UI/Button';
+import { useCopyToClipboard } from '../../hooks/useCopyToClipboard';
+import { HistoryItem } from '../../types';
+
+interface AsciiArtGeneratorProps {
+  onHistoryAdd: (item: Omit<HistoryItem, 'timestamp'>) => void;
+}
+
+type AACharacter = {
+  name: string;
+  category: string;
+  art: string;
+  description?: string;
+};
+
+export function AsciiArtGenerator({ onHistoryAdd }: AsciiArtGeneratorProps) {
+  const [selectedArt, setSelectedArt] = useState('');
+  const [selectedCharacter, setSelectedCharacter] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('all');
+  const { copyToClipboard, isCopied } = useCopyToClipboard();
+
+  const asciiArts: AACharacter[] = [
+    // モナー系
+    {
+      name: 'モナー（基本）',
+      category: 'classic',
+      art: `　　∧＿∧
+　　( ´∀｀)
+　　(　　　 )
+　　｜ ｜ |
+　　（_＿）_）`,
+      description: '2ちゃんねるの元祖マスコット'
+    },
+    {
+      name: 'モナー（挨拶）',
+      category: 'classic',
+      art: `　 ∧＿∧　 ／￣￣￣￣￣￣￣
+　 ( ´∀｀) ＜　おはようモナ〜
+　 (　　　)　 ＼＿＿＿＿＿＿＿
+　 |　| |
+　 （__）_）`
+    },
+    {
+      name: 'モナー（泣き）',
+      category: 'classic',
+      art: `　　 ∧＿∧
+　　 ( ；∀；)
+　　 (　　　 )
+　　 ｜ ｜ |
+　　 （_＿）_）`
+    },
+    // やる夫系
+    {
+      name: 'やる夫',
+      category: 'yaruo',
+      art: `　　　　　　　　　 ／￣￣￣＼
+　　　　　　　　　/　　　　　　　＼
+　　　　　　　　 /　　 ─　　　─　＼
+　　　　　　　　|　　　（●）　（●）　|
+　　　　　　　　 ＼　　　（__人__）　 ／
+　　　　　　　　　 ＼　　　｀ ⌒´　 ／
+　　　　　　　　　　/　　　　　　　　＼`,
+      description: 'VIPの人気キャラクター'
+    },
+    {
+      name: 'やる夫（やった）',
+      category: 'yaruo',
+      art: `　　　　　　　　　 ／￣￣￣＼
+　　　　　　　　　/　　　　　　　＼
+　　　　　　　　 /　　 ─　　　─　＼　　やったお！
+　　　　　　　　|　　　（○）　（○）　|　
+　　　　　　　　 ＼　　　（__人__）　 ／
+　　　　　　　　　 ＼　　　｀ ⌒´　 ／
+　　　　　　　　　　/　　　　　　　　＼`
+    },
+    {
+      name: 'やる夫（困った）',
+      category: 'yaruo',
+      art: `　　　　　　　　　 ／￣￣￣＼
+　　　　　　　　　/　　　　　　　＼
+　　　　　　　　 /　　 ─　　　─　＼　　
+　　　　　　　　|　　　（－）　（－）　|　困ったお...
+　　　　　　　　 ＼　　　（__人__）　 ／
+　　　　　　　　　 ＼　　　｀ ⌒´　 ／
+　　　　　　　　　　/　　　　　　　　＼`
+    },
+    // ギコ系
+    {
+      name: 'ギコ',
+      category: 'giko',
+      art: `　　 ∧∧　　 ／￣￣￣￣￣
+　　 (,,ﾟДﾟ)　＜　ふーん
+　　 /　　｜　　＼＿＿＿＿＿
+　　(___ノ`,
+      description: 'クールなネコキャラ'
+    },
+    {
+      name: 'ギコ（怒り）',
+      category: 'giko',
+      art: `　　 ∧∧　　 ／￣￣￣￣￣
+　　 (,,｀Д´)　＜　ゴルァ！！
+　　 /　　｜　　＼＿＿＿＿＿
+　　(___ノ`
+    },
+    {
+      name: 'ギコ（驚き）',
+      category: 'giko',
+      art: `　　 ∧∧　　 ／￣￣￣￣￣
+　　 (,,ﾟoﾟ)　＜　なんだって！
+　　 /　　｜　　＼＿＿＿＿＿
+　　(___ノ`
+    },
+    // オエー系
+    {
+      name: 'オエー鳥',
+      category: 'oee',
+      art: `　　　 ∧ ∧　　　　／￣￣￣￣￣￣￣￣
+　　　（ ´∀｀）　　＜　オエーーー！！
+　 　 ノ　つ　つ　　　＼＿＿＿＿＿＿＿＿
+　　 ⊂､ ノ　　　　
+　　　　し'`,
+      description: '吐き気を表現するAA'
+    },
+    {
+      name: 'オエー鳥（大）',
+      category: 'oee',
+      art: `　　　　　　　∧ ∧　　　　／￣￣￣￣￣￣￣￣￣
+　　　　　　　（ ´∀｀）　　＜　オエーーーーーー！！
+　　　　　　ノ　つ　つ　　　＼＿＿＿＿＿＿＿＿＿
+　　　　　⊂､ ノ　　　　　
+　　　　　　　し'`
+    },
+    // 麻呂系
+    {
+      name: '麻呂',
+      category: 'maro',
+      art: `　　　　　　　　　 ／⌒＼
+　　　　　　　　　/　　　 ）
+　　　　　　　　 |　　　 /
+　　　　　　　　 |　　 （　/⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒
+　　　　　　　　 |　　　　（　それでおじゃるか？
+　　　　　　　　 |　　　　　⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒
+　　　　　 ＿＿ |　　　　ノ
+　　　　　(　 __|___／
+　　　　　/　　　　 　　＼
+　　　　/　　⌒　　　⌒　　＼
+　　　 |::::::::　（・）　　（・）　　|
+　　　 |::::::::::　　 ＼___／　　 |
+　　　 |::::::::::::::::　　＼/　 　ノ`,
+      description: '平安時代の貴公子風キャラ'
+    },
+    {
+      name: '麻呂（驚き）',
+      category: 'maro',
+      art: `　　　　　　　　　 ／⌒＼
+　　　　　　　　　/　　　 ）
+　　　　　　　　 |　　　 /
+　　　　　　　　 |　　 （　/⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒
+　　　　　　　　 |　　　　（　なんですとおおお！？
+　　　　　　　　 |　　　　　⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒
+　　　　　 ＿＿ |　　　　ノ
+　　　　　(　 __|___／
+　　　　　/　　　　 　　＼
+　　　　/　　⌒　　　⌒　　＼
+　　　 |::::::::　（○）　　（○）　　|
+　　　 |::::::::::　　 ＼___／　　 |
+　　　 |::::::::::::::::　　＼/　 　ノ`
+    },
+    // その他の人気キャラ
+    {
+      name: 'しぃ',
+      category: 'other',
+      art: `　　　 ∧ ∧
+　　　（*ﾟーﾟ)
+　 　 |つ つ
+　　～|　 |
+　　　 Ｕ"Ｕ`,
+      description: '可愛い女の子キャラ'
+    },
+    {
+      name: 'でぃ',
+      category: 'other',
+      art: `　　　 ∧ ∧
+　　　（,, ﾟ∀ﾟ)　　そうでぃ
+　 　 |つ つ
+　　～|　 |
+　　　 Ｕ"Ｕ`
+    },
+    {
+      name: 'フサギコ',
+      category: 'other',
+      art: `　　　∧∧
+　　 ( ﾟ∀ﾟ)　　　んが？
+　　 /　 つつ
+　　　~|　|
+　　　 ∪∪`
+    },
+    {
+      name: 'ぽこたん',
+      category: 'other',
+      art: `　　　　　　　　　　　 ぽこたん
+　　　　∩___∩　　　　　　 いんしたお…
+　　　　|　ノ　　　　 ヽ
+　　　 /　　●　　　● |
+　　　|　　　　( _●_)　 ミ
+　　　彡､　　　|∪|　　 ､｀＼
+　　 /　＿＿　 ヽノ　/´>　 )
+　　(＿＿＿）　　　　/　(_／
+　　　|　　　　　　　 /
+　　　|　　/＼　　　＼
+　　　|　/　　 )　　　 )
+　　　∪　　 （　　　 ＼
+　　　　　　　 ＼＿）`
+    },
+    // 感情表現系
+    {
+      name: 'ズコー',
+      category: 'emotion',
+      art: `　　　　　　　　　　　∧＿∧
+　　　　　　　　　　 （　　´Д｀）
+　　　　　　　　　　 /　　　　 ＼
+　　　　　　　　　　|　l　　 　　 l　|　　　　 ＿＿＿＿＿＿＿＿＿＿
+　　　　　　　　　　|　|　　 　　 |　|　　　　 |＿＿＿＿＿＿＿＿＿＿｜
+　　　　　　　　 ￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣
+　　　　　　　　　　　　　　　　　 ズコー`,
+      description: 'ずっこける表現'
+    },
+    {
+      name: 'ガクガクブルブル',
+      category: 'emotion',
+      art: `　　　　　∧ ∧　　　　　 ／￣￣￣￣￣￣
+　　　　　 (；ﾟ∀ﾟ)＝3　 ＜　ｶﾞｸｶﾞｸﾌﾞﾙﾌﾞﾙ
+　　　　　⊂┯⊂ ) 　　　　 ＼＿＿＿＿＿＿
+　　　 　／⌒＼⌒ヽ.
+　　　　 ｀ー'　 ｀ー' `,
+      description: '恐怖の表現'
+    },
+    // 1行系感情表現（汎用性が高い）
+    {
+      name: 'ｷﾀ━━━(ﾟ∀ﾟ)━━━!!',
+      category: 'oneline',
+      art: `ｷﾀ━━━━━━━━━━━━━━━━━━(ﾟ∀ﾟ)━━━━━━━━━━━━━━━━━━!!!!!`,
+      description: '興奮・喜びの表現'
+    },
+    {
+      name: 'ｷﾀｰ',
+      category: 'oneline',
+      art: `ｷﾀ━━━ヽ(∀ﾟ )人(ﾟ∀ﾟ)人( ﾟ∀)ノ━━━!!`,
+      description: '大勢で喜ぶ表現'
+    },
+    {
+      name: 'うはｗｗｗおkｗｗｗ',
+      category: 'oneline',
+      art: `うはｗｗｗおkｗｗｗ`,
+      description: '2ちゃん特有の表現'
+    },
+    {
+      name: 'ワロタ',
+      category: 'oneline',
+      art: `ワロタ`,
+      description: '笑った時の表現'
+    },
+    {
+      name: 'ワロスwww',
+      category: 'oneline',
+      art: `ワロスwwwwwwwww`,
+      description: '爆笑の表現'
+    },
+    {
+      name: 'ちょwwwおまwww',
+      category: 'oneline',
+      art: `ちょwwwおまwwwwww`,
+      description: '突っ込みの表現'
+    },
+    {
+      name: '(´･ω･\`)ショボーン',
+      category: 'oneline',
+      art: '(´･ω･\`)ショボーン',
+      description: '落ち込み・がっかり'
+    },
+    {
+      name: '( ´Д｀)ﾊｧｧ',
+      category: 'oneline',
+      art: `( ´Д｀)ﾊｧｧ`,
+      description: 'ため息・疲れ'
+    },
+    {
+      name: '工エエェェ(´д｀)ェェエエ工',
+      category: 'oneline',
+      art: `工エエェェ(´д｀)ェェエエ工`,
+      description: '驚き・困惑'
+    },
+    {
+      name: 'Σ(ﾟДﾟ)ｽｹﾞｰ',
+      category: 'oneline',
+      art: `Σ(ﾟДﾟ)ｽｹﾞｰ!!`,
+      description: '衝撃・驚愕'
+    },
+    {
+      name: '(・∀・)ｲｲ!',
+      category: 'oneline',
+      art: `(・∀・)ｲｲ!`,
+      description: '良い・賛成'
+    },
+    {
+      name: '(・A・)ｲｸﾅｲ!',
+      category: 'oneline',
+      art: `(・A・)ｲｸﾅｲ!`,
+      description: '悪い・反対'
+    },
+    {
+      name: '(´∀｀)ｱﾊﾊ八八ﾉヽﾉヽﾉヽﾉ ＼ / ＼/ ＼',
+      category: 'oneline',
+      art: `(´∀｀)ｱﾊﾊ八八ﾉヽﾉヽﾉヽﾉ ＼ / ＼/ ＼`,
+      description: '大爆笑'
+    },
+    {
+      name: 'm9(^Д^)ﾌﾟｷﾞｬｰ',
+      category: 'oneline',
+      art: `m9(^Д^)ﾌﾟｷﾞｬｰ`,
+      description: '指差して笑う'
+    },
+    {
+      name: '( ´∀｀)σ)Д`)ﾌﾟﾆｮｰﾝ',
+      category: 'oneline',
+      art: '( ´∀\`)σ)Д\`)ﾌﾟﾆｮｰﾝ',
+      description: '頬つねり'
+    },
+    {
+      name: '(ﾉ∀`)ｱﾁｬｰ',
+      category: 'oneline',
+      art: '(ﾉ∀\`)ｱﾁｬｰ',
+      description: '失敗を見て笑う'
+    },
+    {
+      name: '(´・ω・`)知らんがな',
+      category: 'oneline',
+      art: '(´・ω・\`)知らんがな',
+      description: '関西弁でつっこみ'
+    },
+    {
+      name: '(ﾟДﾟ)ﾊｧ?',
+      category: 'oneline',
+      art: `(ﾟДﾟ)ﾊｧ?`,
+      description: '疑問・困惑'
+    },
+    {
+      name: '(　´∀｀)σ)∀`)ｸｽｸｽ',
+      category: 'oneline',
+      art: '(　´∀\`)σ)∀\`)ｸｽｸｽ',
+      description: 'くすくす笑い'
+    },
+    {
+      name: '(・∀・)ｽﾝｽﾝｽｰﾝ',
+      category: 'oneline',
+      art: `(・∀・)ｽﾝｽﾝｽｰﾝ♪`,
+      description: '楽しい・上機嫌'
+    },
+    {
+      name: '(゜∀゜)ｱﾋｬ',
+      category: 'oneline',
+      art: `(゜∀゜)ｱﾋｰ`,
+      description: '狂気の笑い'
+    },
+    {
+      name: '(￣ー￣)ﾆﾔﾘ',
+      category: 'oneline',
+      art: `(￣ー￣)ﾆﾔﾘ`,
+      description: '意味深な笑み'
+    },
+    {
+      name: '＿|￣|○',
+      category: 'oneline',
+      art: `＿|￣|○`,
+      description: '土下座・謝罪'
+    },
+    {
+      name: 'orz',
+      category: 'oneline',
+      art: `orz`,
+      description: '絶望・挫折'
+    },
+    {
+      name: 'OTL',
+      category: 'oneline',
+      art: `OTL`,
+      description: 'orzの変形'
+    },
+    {
+      name: '○| ￣|＿',
+      category: 'oneline',
+      art: `○| ￣|＿`,
+      description: 'orzの逆'
+    },
+    {
+      name: 'Σ(゜Д゜;)',
+      category: 'oneline',
+      art: `Σ(゜Д゜;)`,
+      description: 'ビックリ・焦り'
+    },
+    {
+      name: '(-_-;)',
+      category: 'oneline',
+      art: `(-_-;)`,
+      description: '困った・呆れ'
+    },
+    {
+      name: '(^o^)/',
+      category: 'oneline',
+      art: `(^o^)/`,
+      description: '元気・万歳'
+    },
+    {
+      name: '＼(^o^)／',
+      category: 'oneline',
+      art: `＼(^o^)／`,
+      description: '両手上げて喜び'
+    },
+    {
+      name: 'ヽ(´ー` )ノ',
+      category: 'oneline',
+      art: 'ヽ(´ー\` )ノ',
+      description: 'のんびり・マッタリ'
+    },
+    {
+      name: '( ・ω・)ﾓﾆｭ?',
+      category: 'oneline',
+      art: `( ・ω・)ﾓﾆｭ?`,
+      description: '疑問・首かしげ'
+    },
+    {
+      name: '(　´ Д ｀ )y-~~',
+      category: 'oneline',
+      art: `(　´ Д ｀ )y-~~`,
+      description: 'タバコを吸う'
+    },
+    {
+      name: 'ヽ(ﾟ∀ﾟ)ﾉ',
+      category: 'oneline',
+      art: `ヽ(ﾟ∀ﾟ)ﾉ`,
+      description: '喜び・やったー'
+    },
+    {
+      name: '(*´д｀*)ハァハァ',
+      category: 'oneline',
+      art: `(*´д｀*)ハァハァ`,
+      description: '興奮・息切れ'
+    },
+    {
+      name: '(ﾟ∀ﾟ)ｱﾋｬ',
+      category: 'oneline',
+      art: `(ﾟ∀ﾟ)ｱﾋｬ`,
+      description: '壊れた笑い'
+    },
+    {
+      name: 'щ(゜Д゜щ)',
+      category: 'oneline',
+      art: `щ(゜Д゜щ)`,
+      description: 'カモーン'
+    },
+    {
+      name: '(((( ；゜Д゜)))',
+      category: 'oneline',
+      art: `(((( ；゜Д゜))))ガクガクブルブル`,
+      description: '恐怖で震える'
+    },
+    {
+      name: '(´Д⊂)',
+      category: 'oneline',
+      art: `(´Д⊂)`,
+      description: '涙・感動'
+    },
+    {
+      name: '(つД⊂)',
+      category: 'oneline',
+      art: `(つД⊂)`,
+      description: 'もっと泣き'
+    },
+    {
+      name: '(´；ω；`)',
+      category: 'oneline',
+      art: '(´；ω；\`)',
+      description: '大泣き'
+    }
+  ];
+
+  const categories = [
+    { value: 'all', label: 'すべて' },
+    { value: 'classic', label: 'クラシック' },
+    { value: 'yaruo', label: 'やる夫' },
+    { value: 'giko', label: 'ギコ' },
+    { value: 'oee', label: 'オエー' },
+    { value: 'maro', label: '麻呂' },
+    { value: 'other', label: 'その他キャラ' },
+    { value: 'emotion', label: '感情表現' }
+  ];
+
+  const getFilteredArts = () => {
+    if (selectedCategory === 'all') {
+      return asciiArts;
+    }
+    return asciiArts.filter(art => art.category === selectedCategory);
+  };
+
+  const generateRandomArt = () => {
+    const filteredArts = getFilteredArts();
+    if (filteredArts.length === 0) return;
+    
+    const randomIndex = Math.floor(Math.random() * filteredArts.length);
+    const selectedAA = filteredArts[randomIndex];
+    
+    setSelectedArt(selectedAA.art);
+    setSelectedCharacter(selectedAA.name);
+    
+    onHistoryAdd({
+      toolId: 'ascii-art-generator',
+      result: `${selectedAA.name}を生成`
+    });
+  };
+
+  const selectSpecificArt = (character: AACharacter) => {
+    setSelectedArt(character.art);
+    setSelectedCharacter(character.name);
+    
+    onHistoryAdd({
+      toolId: 'ascii-art-generator',
+      result: `${character.name}を選択`
+    });
+  };
+
+  const handleCopy = () => {
+    if (selectedArt) {
+      copyToClipboard(selectedArt);
+    }
+  };
+
+  const filteredArts = getFilteredArts();
+
+  return (
+    <div className="space-y-6">
+      <div className="space-y-4">
+        <div className="flex flex-col md:flex-row gap-4">
+          <div className="flex-1">
+            <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
+              カテゴリ選択
+            </label>
+            <select
+              value={selectedCategory}
+              onChange={(e) => setSelectedCategory(e.target.value)}
+              className="w-full p-2 border border-gray-300 dark:border-gray-600 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-white text-sm"
+            >
+              {categories.map(category => (
+                <option key={category.value} value={category.value}>
+                  {category.label}
+                </option>
+              ))}
+            </select>
+          </div>
+          
+          <div className="flex gap-2 items-end">
+            <Button onClick={generateRandomArt} className="whitespace-nowrap">
+              ランダム生成
+            </Button>
+            {selectedArt && (
+              <Button onClick={handleCopy} variant="outline">
+                {isCopied ? 'コピー済み!' : 'コピー'}
+              </Button>
+            )}
+          </div>
+        </div>
+
+        <div className="text-sm text-gray-600 dark:text-gray-400">
+          <p>
+            <strong>対象キャラ:</strong> {filteredArts.length}個のAAが利用可能
+            {selectedCharacter && ` | 現在表示: ${selectedCharacter}`}
+          </p>
+        </div>
+      </div>
+
+      {selectedArt && (
+        <div className="space-y-4">
+          <div className="flex items-center justify-between">
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              生成されたAA
+            </h3>
+          </div>
+
+          <div className="bg-gray-50 dark:bg-gray-800 p-4 rounded-lg border border-gray-300 dark:border-gray-600">
+            <pre className="text-sm font-mono text-gray-900 dark:text-white whitespace-pre overflow-x-auto">
+{selectedArt}
+            </pre>
+          </div>
+        </div>
+      )}
+
+      <div className="space-y-4">
+        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+          キャラクター一覧（{categories.find(c => c.value === selectedCategory)?.label}）
+        </h3>
+        
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {filteredArts.map((character, index) => (
+            <div
+              key={index}
+              className="bg-white dark:bg-gray-800 p-3 rounded-lg border border-gray-300 dark:border-gray-600 hover:bg-gray-50 dark:hover:bg-gray-700 cursor-pointer transition-colors"
+              onClick={() => selectSpecificArt(character)}
+            >
+              <div className="font-medium text-gray-900 dark:text-white mb-1 text-sm">
+                {character.name}
+              </div>
+              {character.description && (
+                <div className="text-xs text-gray-600 dark:text-gray-400 mb-2">
+                  {character.description}
+                </div>
+              )}
+              <pre className="text-xs font-mono text-gray-700 dark:text-gray-300 whitespace-pre overflow-hidden">
+{character.art.split('\n').slice(0, 3).join('\n')}
+{character.art.split('\n').length > 3 ? '...' : ''}
+              </pre>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="text-sm text-gray-600 dark:text-gray-400">
+        <p><strong>使い方:</strong></p>
+        <ul className="list-disc list-inside space-y-1 mt-1">
+          <li><strong>ランダム生成:</strong> 選択されたカテゴリからランダムにAAを表示</li>
+          <li><strong>個別選択:</strong> キャラクター一覧から好きなAAをクリック</li>
+          <li><strong>コピー:</strong> 生成されたAAをクリップボードにコピー</li>
+          <li><strong>カテゴリ:</strong> モナー、やる夫、ギコなど人気キャラで絞り込み</li>
+        </ul>
+        <p className="mt-2">
+          <strong>注意:</strong> AAは2ちゃんねる文化の一部として教育・文化的な目的で提供しています。
+        </p>
+      </div>
+    </div>
+  );
+}
